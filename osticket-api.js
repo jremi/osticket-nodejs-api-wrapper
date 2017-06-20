323bbc60e14ed6ef744ae7035fb04dd8599a1ed0
Jremi <jeremy.bursky@gmail.com>
2017-06-20 14:50:33 -0700

    Added request npm dependency.

--- osticket-api.js
+++ osticket-api.js
@@ -1,25 +1,25 @@
 module.exports = function (osTicketAPI_settings, formData, callback) {
     var request = require('request'),
-    var requestData = {
-        "alert" : osTicketAPI_settings.ALERT,
-        "autorespond" : osTicketAPI_settings.AUTO_RESPOND, 
-        "source" : "API", 
-        "name" : formData.name,
-        "email" : formData.email, 
-        "subject" : formData.subject, 
-        "message" : formData.message,
-        "topicId" : function(){
-            // Here you can setup your different Help Topics. They must be setup in osTicket.
-            // Set the return value to the id number of the topic that was generated in osTicket.
-            // For example:
-            switch(formData.topicId){
-                case "General Questions":
-                    return 1;
-                case "Other":
-                    return 2;
-            }   
-        }()
-    };
+        requestData = {
+            "alert" : osTicketAPI_settings.ALERT,
+            "autorespond" : osTicketAPI_settings.AUTO_RESPOND, 
+            "source" : "API", 
+            "name" : formData.name,
+            "email" : formData.email, 
+            "subject" : formData.subject, 
+            "message" : formData.message,
+            "topicId" : function(){
+                // Here you can setup your different Help Topics. They must be setup in osTicket.
+                // Set the return value to the id number of the topic that was generated in osTicket.
+                // For example:
+                switch(formData.topicId){
+                    case "General Questions":
+                        return 1;
+                    case "Other":
+                        return 2;
+                }   
+            }()
+        };
     request.post({
         url: osTicketAPI_settings.INSTALL_URL_PATH + '/api/http.php/tickets.json',
         json: true,
